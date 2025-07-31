241c2db8ea63160162f4481be217ea8b0ebc9424
Gabriel Dokov <gabriel.dokovv991@gmail.com>
2025-07-31 15:09:15 +0300

    remove README created from CRA and remove Fragment in EditUserForm

--- react-user-list-task/src/components/EditPostForm/EditPostForm.tsx
+++ react-user-list-task/src/components/EditPostForm/EditPostForm.tsx
@@ -24,78 +24,76 @@ const EditPostForm = ({ post, isOpen, onClose, notificationApi }: Props) => {
   };
 
   return (
-    <>
-      <Formik
-        initialValues={post}
-        validationSchema={validationSchema}
-        enableReinitialize
-        onSubmit={handleEditPost}
-        validateOnChange={false}
-        validateOnBlur={true}
-      >
-        {({
-          dirty,
-          isValid,
-          errors,
-          values,
-          handleReset,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-        }) => (
-          <Modal
-            open={isOpen}
-            width={700}
-            onCancel={() => {
-              handleReset();
-              onClose();
-            }}
-            footer={
-              <>
-                <Button
-                  onClick={() => {
-                    handleReset();
-                    onClose();
-                  }}
-                >
-                  Cancel
-                </Button>
-                <Button onClick={() => handleSubmit()} disabled={!dirty || !isValid} type="primary">
-                  Submit
-                </Button>
-              </>
-            }
-            title={"Edit User Post"}
-          >
-            <Form>
-              <Row gutter={24}>
-                <Col span={24}>
-                  <Form.Item label="Title" validateStatus={errors.title ? "error" : ""} required>
-                    <Input
-                      name="title"
-                      value={values.title}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                    />
-                  </Form.Item>
-                </Col>
-                <Col span={24}>
-                  <Form.Item label="Body" validateStatus={errors.body ? "error" : ""} required>
-                    <Input.TextArea
-                      rows={3}
-                      name="body"
-                      value={values.body}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                    />
-                  </Form.Item>
-                </Col>
-              </Row>
-            </Form>
-          </Modal>
-        )}
-      </Formik>
-    </>
+    <Formik
+      initialValues={post}
+      validationSchema={validationSchema}
+      enableReinitialize
+      onSubmit={handleEditPost}
+      validateOnChange={false}
+      validateOnBlur={true}
+    >
+      {({
+        dirty,
+        isValid,
+        errors,
+        values,
+        handleReset,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+      }) => (
+        <Modal
+          open={isOpen}
+          width={700}
+          onCancel={() => {
+            handleReset();
+            onClose();
+          }}
+          footer={
+            <>
+              <Button
+                onClick={() => {
+                  handleReset();
+                  onClose();
+                }}
+              >
+                Cancel
+              </Button>
+              <Button onClick={() => handleSubmit()} disabled={!dirty || !isValid} type="primary">
+                Submit
+              </Button>
+            </>
+          }
+          title={"Edit User Post"}
+        >
+          <Form>
+            <Row gutter={24}>
+              <Col span={24}>
+                <Form.Item label="Title" validateStatus={errors.title ? "error" : ""} required>
+                  <Input
+                    name="title"
+                    value={values.title}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={24}>
+                <Form.Item label="Body" validateStatus={errors.body ? "error" : ""} required>
+                  <Input.TextArea
+                    rows={3}
+                    name="body"
+                    value={values.body}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+        </Modal>
+      )}
+    </Formik>
   );
 };
 
