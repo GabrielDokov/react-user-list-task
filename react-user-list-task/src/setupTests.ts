3edd6c15f1738580e860ab92ec8e8b3a60c90158
Gabriel Dokov <gabriel.dokovv991@gmail.com>
2025-08-03 14:13:09 +0300

    mock i18next for tests and update snapshot

--- react-user-list-task/src/setupTests.ts
+++ react-user-list-task/src/setupTests.ts
@@ -3,7 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-
 import { TextEncoder, TextDecoder } from "util";
 
 Object.assign(global, { TextDecoder, TextEncoder });
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+    };
+  },
+  initReactI18next: {
+    type: "mock",
+    init: jest.fn(),
+  },
+}));
